49a366b37e3928166608fd9feeeb31ff53e9dafa
divyashah1505 <divya.1505shah@gmail.com>
2026-05-06 15:57:58 +0530

    first commits

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,1 @@
-// export const BASE_URL = "https://e-learnig-recipebackend.onrender.com";
-// const BASE_URL = process.env.REACT_APP_BACKEND_URL;
-export const BASE_URL = "http://localhost:3000/api/users";
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
